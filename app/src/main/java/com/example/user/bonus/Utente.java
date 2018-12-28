963038fafefff945bf416656a4a83dabe5f82406
Luca Grassi <37103375+Luca14797@users.noreply.github.com>
2018-12-28 19:02:34 +0100

    Creazione prima schermata

---
+++ app/src/main/java/com/example/user/bonus/Utente.java
@@ -0,0 +1,29 @@
+package com.example.user.bonus;
+
+import java.io.Serializable;
+
+public class Utente implements Serializable {
+    private String nomeUtente;
+    private String password;
+
+    public Utente (String nomeUtente, String password) {
+        this.nomeUtente = nomeUtente;
+        this.password = password;
+    }
+
+    public String getNomeUtente () {
+        return this.nomeUtente;
+    }
+
+    public String getPassword () {
+        return this.password;
+    }
+
+    public void setNomeUtente (String nomeUtente) {
+        this.nomeUtente = nomeUtente;
+    }
+
+    public void setPassword (String password) {
+        this.password = password;
+    }
+}
