4bac0e13152aa221a39af5827c5662741ba81109
Luca Grassi <37103375+Luca14797@users.noreply.github.com>
2018-12-31 12:58:21 +0100

    Manca il link "Nuovo Utente"

--- app/src/main/java/com/example/user/bonus/Main2Activity.java
+++ app/src/main/java/com/example/user/bonus/Main2Activity.java
@@ -1,8 +1,13 @@
 package com.example.user.bonus;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.Serializable;
 
@@ -10,6 +15,8 @@ public class Main2Activity extends AppCompatActivity {
 
     Utente utente;
     Utente[] utenti = {new Utente("Luca","Ciao"), new Utente("Nicola","Miao")};
+    TextView testoLogin;
+    Button indietro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,28 @@ public class Main2Activity extends AppCompatActivity {
         } else {
             utente = new Utente();
         }
+
+        testoLogin = (TextView) findViewById(R.id.textViewResult);
+        indietro = (Button) findViewById(R.id.indietro);
+
+        ColorStateList defaultColor = testoLogin.getTextColors();
+
+        if (login()) {
+            testoLogin.setTextColor(defaultColor);
+            testoLogin.setText("Benvenuto" + " " + utente.getNomeUtente());
+        } else {
+            testoLogin.setTextColor(Color.RED);
+            testoLogin.setText("Accesso Negato");
+        }
+
+        indietro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                Intent showResults = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(showResults);
+            }
+        });
     }
 
     private boolean login() {
--- app/src/main/java/com/example/user/bonus/MainActivity.java
+++ app/src/main/java/com/example/user/bonus/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.user.bonus;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     Button accedi;
     Utente utente;
 
-    public static final String OK_LOGIN = "com.example.user.bonus";
+    public static final String OK_LOGIN = "com.example.user.bonus.Utente";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (checkInput()) {
-                    utente = new Utente(username.toString(), password.toString());
+                    utente = new Utente(username.getText().toString(), password.getText().toString());
                     Intent showResults = new Intent(MainActivity.this, Main2Activity.class);
                     showResults.putExtra(OK_LOGIN, utente);
                     startActivity(showResults);
